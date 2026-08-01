7d960b7d0c6332e13ec9ecdd66992d3e025d967c
APMaii <ali.pilehvarmeibody@studenti.polito.it>
2026-08-01 18:20:07 +0330

    ad l1 adv

---
+++ L1_ADV.py
@@ -0,0 +1,708 @@
+"""
+Created on Sun Jul 26 20:19:26 2026
+
+@author: apm
+
+
+
+
+
+
+
+
+Human (en) -----> Machine (0,1 binary)
+
+
+dastoor --> Programming --> Barname nevisi
+
+
+
+human (en)   -----> Machine(0,1 Binary)
+
+Human (en) <-----Vasete ----> Machine(0,1 Binary)
+
+Mokhafaf hayemokhtalef misazan, Vasete --> Programming language (python, C , C++ , JS , ...)
+
+
+open file --> 0010010010101010110100101010  --> open() , op() , OOT[]
+Namayesh bedid --> 001001010111111011100111 --> print() , scanf() , console() 
+
+
+
+
+
+kASI K code mizane --> Programmer -->
+
+company , startup 
+
+
+Basic User --> Shoro kardi
+
+Junior --> Javan --> yad migire (1 sal gozashte)
+
+Intermediate (2-4 sal) --> Sathe motevaset
+
+Senior --> (6,7 sal --> 12 sal) 
+
+Tech lead ---> (hadeaghal 15 sal)
+
+
+
+
+Programmer --> Intermediate , chizhaee k inja yad grftid 
+mohem nist anchenan chon shoma az sefr code nemizanid
+
+baraye sakhte yek mashine jadid, az aval charkh nemisazan,
+
+ma yekseri ketabkhone darim (library, package) k dakheelshon hezzaran class, Function 
+in ha code haye 100 khat, 400 khat, 500 khat inaro shamel mishe
+
+niaz nis bshini 400 khat code bzni , oon tabe ro seda bzni 
+
+az yeja bbad shoam kolan karet yadgirie package (library) , usage 
+
+
+va badesh app --> 40 ta ketabkhane mokhtalef
+
+har ketabkhone --> organization, company , community
+
+va harkodom application haye mokhtalefi daran (fielde mokhtalef hastan)
+
+
+numpy --> mohasebate riazi --> NASA -->calculation Numpy , Do meidani mohaseebeye secodn dakhele programm chip --> numpy ,....
+
+Scipy , Sympy --> differential , jabri hast 
+
+Pandas --> Kar ba data (csv , excell) --> data scientist ha --> data cleaning
+
+Matplotlib --> rasm , mosaver sazi
+
+Sklearn (Scikit-learn) --> Mohem tarin ketabkhane Machine learning 
+
+
+Tensorflow  --> Google (deepmind) --> yadgirie amigh (deep learning)
+Pytorch  --> Meta (facebook,insta,whatsapp) --> deep learning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Computer az sefr betonam toosh barname nevisi konam b do chiz niaz dare
+
+1- Python kernel --> Yek motore python hast k man python ro miad tarjome mikone be binary (zabane computer
+
+            
+Man koja benevisam codam ro k befrestam baraye kernel ta run kone??
+
+2- Mohite code nevisi --> Code Editor , IDE (integratd development environment)                                                                               
+
+
+
+                                                                                           )
+Base tarin halat ----------
+
+ch code editor che IDE yek jaei hastan k shoma codeeton ro minevisid, in ha ersal mikonan
+be python kernel -> tarjome mikone b zabane binary machine --> computer ejra mikone javbo barmigrdoone
+
+
+        Ensan (human) englisi
+             |
+             |
+            \/
+        Python code minevisi (Editor,IDE)
+             |
+             |
+            \/
+        Python Kernel (Python --> Binary)
+             |
+             |
+            \/
+        Machine (Laptab , RAM , CPU) 
+             |
+             |
+            \/
+        Javab ro bar migrdone
+             |
+             |
+            \/
+    IDE , EDITOR behet namayesh mide
+
+
+
+
+
+Do ravesh darid k betonid dar yek latobe sefr code zani ro shoro konid
+
+
+
+1- Raveshe aval 
+
+b do chiz niaz darid
+1.1. Python kernel bayad bashe k befahme python ro va tarjomash kone b binary 
+
+1.2. Yek mohitri k toosh code bznid
+
+
+python.org --> download Python --> 3.14.7 
+3.8 b bala 
+
+
+(((((( 
+Tamame barname ha se adad daran baraye versioning (version gozashtanesh)
+_ , _ , _
+_ . _ . _ 
+adade samte chape --> versione aslie app e , app versio 2......  , 3...... az zamin ta asemon fargh daran
+python 2 , python 3 az zamin ta asemon frgh dre 
+
+major,minor,patch
+
+
+
+
+_._  ADADE DOVOM--> feature (vizhegi dare)
+
+3.1. --> ye vizhegie kahsi
+
+3.2. --> 4 ta vzihegi ezafe krdn
+
+3.4 -->vizhegi
+
+
+kar mikone , versione 3.1. vizhegi haye 3.4 
+
+
+
+3.4.0 --> avalin versionete
+
+avalin buggi  mikhore , issue report ,... debugg , fix , solve problem 
+
+release -> montasher 
+
+3.4.1   --> changes : ......
+3.4.2. 
+3.4.3.
+3.4.4. 
+
+))))))
+
+
+
+1.2-->Yek mohiti baraye code nevisi --> Spyder , VS code --> website
+spyde 3.14.2  4.16.7  download mikoni
+
+
+
+IDE/Editor baz mikoni codeto minevisi 
+
+sade tarin ravesh hast  (Fast, easy, not professional, and bottleneck )
+
+
+python 3.14
+Numpy 3.1
+
+beham dependecy (vabastegi drn)
+
+sklearn 4.17 --> numpy 2.19
+
+
+yek project mizanid app misazi
+sklearn 4.17 , nmumpy 2.16 , ......
+
+
+
+
+dosal dg sklearn 5.13  , numpy 2.50
+update koni laptabeto --> conflict appet run nmishe
+update nakoni --> 
+
+
+
+
+
+
+2- Raveshe dovom (Professional)
+yani bejaye inke kolan hamechito berizi too kole laptabet , kole laptabet
+laptabeto taghsim kon b chand mohit
+
+
+
+----------------
+|               |
+|     Python    |
+|     IDE/ed    | 
+|      Packs    |
+----------------
+
+
+
+------------------
+|  env1 || env2   |
+|       ||       |
+------------------
+|       ||       |
+|  env3 || env4  |
+------------------
+
+
+env1 (app e felani) --> yek version python, yek version spyder,vs code , yek version az package
+
+
+
+
+Environemnt manager berizi -_> modiriate
+
+Anaconda --> website --> filtershekamn , estefadash
+
+
+dakhelesh k miri--> Home (Base kole laptab)
+
+Environements--> create environment --> yek mohite jadid misaz (yek mini laptab dkahele laptab)
+
+python 3.13 , spyder ,....
+
+
+yedone spyder nadari , baraye har mohitet yek spyder dari , yek vs code
+
+
+fanavari405 -> inja kon
+
+project1 -->
+
+project2--->
+
+
+
+
+Editor / IDE
+
+editor , IDE --> dakheelsh code minvisi
+
+
+Editor --> avalin version , VS Code ,(python ) , hame zabani 
+
+yek mohiti hast k shoam codeto benevisio ersal kone be kernel --> hasteye computer 
+kernel C , kernele C++ , kERNELE Python
+
+
+sublime, vs code, cursor ,.....
+
+
+IDE-> man na tanha yek editor daram balke chanta vizhegi ham roosh mziaram
+samte rast , variable , plot , file , console 
+rangamizi , setting,hamzaman k code mizni erroreto mibine
+
+mitoni bejaye inke whole code (tamame codeto run koni (majbori toye Editor)
+                               inja mitoni fght section (yek ghesm ati)
+                               
+                               
+
+
+nesbat b applicationi (karbordi) shoma mitoni az editor ya IDE 
+
+IDE --> bekhatere in hame featuyre --> Memory --> Kond ,.....
+
+yek codo ye negah bendazam , sari mikham y edite sari bznm --> Editor 
+
+beram yek data az khone haye california , price estimation --> IDE
+
+
+Telegram , whatsapp --> ina karehon ine code shoamro befresan b machine
+
+Human ---> IDE/Editor ---> Kernel --> Machine
+
+
+Human1 --> Telegram/whatsapp ---> server ---> DOoostam2
+
+
+Kareshon ychize , yeseri vzihegi 
+
+
+Spyder , pycharm (IDE)--> choon oon rast variables , plot , 
+mitoni done done run koni , in chap mizan e,,....
+
+
+
+
+"""
+
+
+'''
+.py misazid
+
+.txt .doc .docx .mp4 .png 
+
+.py --> baraye python
+
+
+SPYDER/VS code .py ----> kernel ipython ---> Hasteye computer
+
+run nakonid etefaghi nmiofte .
+
+run k mikonid , ipython mesle yek ensan az bala b paein , az chap b rast
+codeton ro mikhone ejra mikone
+
+
+
+harchizi benevsiid ro ejra mikone av bayad barash tarif shdoe bashe vaagrne Error midahad
+
+
+
+man tozih bekham bnvism chikar konm/
+
+space , khate khalio mipare
+
+# bezari mitoni tozih bnvis ignore , skip , sarfe nazar
+
+# --> yek khate
+
+
+
+
+
+
+
+'''
+
+
+#yek khat -->dars bedi , tozih bdi khate codeto l,....
+
+'''
+Inja mitoni multiple line harf bzni
+'''
+
+"""
+Inja mitoni multiple line harf bzni
+
+"""
+
+
+
+
+'''
+
+englisi , faransavi , itlaiaei .,,..... farsi
+
+--------Python --------------
+mishnasam , defined 
+1- Python built in function  (tavabeye dakhelie python)
+Narenji mishe, parantez dashte --> 100 ta hast liste kameli
+
+ina yek fucntioni daran, amalkardi daran
+print() --> namayehs mide
+open()
+len() andaze mide
+type() type ro mide
+input() vorodi migire
+......
+
+
+
+2- Keywords --> zamani k mikhahi logice barname ro taghir bdi
+sharti koni , yekseri khat ha ejra bshe yeseria nashe, dorahi 
+yeseri khat ha chandin bar ejra bshe va va ...
+Banafsh --> if else elif , for , while  , def , and or ,...
+
+
+
+3- Harchizi gheyre python built in function(1) va keywords (2) benevisi , sefid minevisatesh
+undefined nemishanas --> be onvane ** Esme zarf dar nazar migire**
+
+Variable (moteghayer) --> toosh value (meghdar) mirize
+
+
+variabel --> zarf yek zarf drm toosh yek meghdar hast
+
+man  cod eminvism hezaran zarf misazam, khob baraye dastresi behesho oon zarf in zarf na onyeki zarf
+esm bzaram --> esme zarf , esme variable
+
+------------
+| Variable |  ------> Name, type , size
+-------------
+|          |
+|  Value   |
+|----------|
+
+
+zarf --> meghdar hast 
+
+
+
+
+
+
+
+
+'''
+
+
+#------1Python built in fucntions
+
+
+print('Hello world!')
+
+#prrrint()
+
+
+len('ali') #3
+
+type(10) #int
+
+#100 ta python built in functions.....
+
+
+
+#-----2- Keywordss ----Mantghe
+#mantegh --> az bala b paeen az chap b rast
+#done done run kon
+
+
+#alan manteghe code excution (ejraye code r)taghir
+
+
+a=10 #a=20 a=40 a=2
+print('salam')
+
+a=10
+if a>18:
+    print('salam')
+
+
+for i in range(0,10):
+    print('salam')
+
+
+#keyword --> yek chize banafshe , structure (sakhtar)
+
+
+#-----Undefined (sefi)----------
+#----3- Variables---------------
+#---3.1. Numbers (Integer, Float,complex)
+
+#3.1.1 integer sahih (-binahyat, -4,-3,..0,1,2,..)
+
+a = 10
+
+#3.1.2 Float (ashar)
+
+b = 10.7663223
+
+#3.1.3. complex
+c = 1j 
+
+# 2 nokte 
+
+#overwrite -->
+
+b = 40
+
+b = 70
+
+#b = 40 , b = , 40 ro briz door , 70 ro beriz dakhelesh
+#overwrite
+
+#do --> 
+
+
+
+
+# harkodom k chap minevise ag nabashe misaze , ag bashe khali mikone ***
+a= 10
+b=40
+a=b #a=40 , b =40  . a ro khali krd , megdhare b ro rikht toosh  
+
+
+
+
+
+
+
+a= 10
+b=40
+b = a  #b ro khali mikrd, a=10 ro dakhelesh mriikht --> b = 10 , a=10
+
+
+
+# a + b = c 
+
+#aval zarf ro beszim , bad operation anajm bdimo berizim dakhelesh
+#yek zarde c besaz ya agar bod khalish kon 
+#a ro be alave b kon beriz dakhelesh
+
+c = a + b 
+
+
+
+#mathematical operations 
+c = a **b #tavan
+c = a *  b #zarb
+c = a /  b  #taghsim
+c = a +  b  #jam
+c = a -  b  #tafrigh
+
+#() > tavan > zarb=taghsm > jam=tafrigh
+
+
+c= a**2+5  #a**2  + 5  || #a**7 ?
+
+c = (a**2) + 5 
+
+c = a**(2+5)
+
+
+# comparisonal Operations
+#ina soalie
+#== 
+#!=
+#> >=
+#< <=
+#ina dastor nmidi
+
+c = a + b
+#a ro b alave b kon (dastoor) beriz tooye c (dastoor)
+
+#aya a = ast ba b ? soale --> montazere javabiu
+
+#bale kheyr , ari nakheyr?
+
+#True , False
+print(a==b) #False
+
+c = a>b
+
+print(c) #False
+
+type(c) #boool
+
+
+
+#-----
+#3.Variables
+#3.1.Numbers (int, float, complex)
+#3.2. Bool (True, False)
+#3.3. Str (string ha hastan)
+
+
+
+
+
+a  = 10 
+b = 30
+c = a
+
+
+print(c) #10 
+
+#yani dar ZARFE c , boro megdhare ZARFE a ro beriz
+
+
+
+#kalamey a ro dar c zakhire konm
+
+
+#az koja python tashkhis bde man manzorm yek esme zarfe ya yek megdhar
+
+#qutation --> ejaze bede shoma har kalme ee , har horofi , characteri,. adadi toye keyboardet hast ro zakhire koni
+
+#reshte ee az character ha --> string
+
+c = 'a'
+
+#dar zarfi bename C , harfe a ro zakhire kon
+
+
+name = 'ali'
+
+print(name) #meghdare tooye zarfe name ro bhm bde
+
+
+
+a = 2
+b = '2'
+
+print(a==b) #False
+
+type(a) #int
+type(b) #str
+
+
+'''
+zarrf --> name, type , size, value
+
+agar dota zarf (variable) bekahn barabar bashan
+
+value bayad baerabar bashe, size, type ham bayad barabar bashe
+
+
+2 
+
+'2' yek meghdaran
+
+type motefavet hst
+
+
+
+'''
+
+
+
+a = 2
+b = '2'
+
+
+
+print(a/100) #0.02
+
+print(b/100) #TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+#in 2 e adadi nsit, in yek character az keyboardam hast
+
+name = 'ali'
+print(name/1023)
+#TypeError: unsupported operand type(s) for /: 'str' and 'int'
+
+
+
+
+
+
+'''
+Pythion built in fucntions --> tedadisho migim
+Varede str functions --> ziad dardim
+
+
+str function--> 4 tya fucntion kh moheme
+
+say daram dar yek mesale mini project --> website amazon,divar, digikala 
+
+yek feature bakhsh k foroshande vaghty yek kalasho mikhad varede system kone
+codesh b che shekl hast va ma chegone aaz concept haee k yad grftim mitonim estefade konim
+
+b sehdat mohem mibashad.
+
+
+ink hafteye ayande bashe ya hafte ye badesh (Dynamic) nesbat b tadrisi khanome davodian
+
+hamishe yekshanbe 8 - 10 mibaashad class
+
+1 hafte dar mioon
+se shaneb b hameye azizan etela resani konm
+
+
+'''
+
+
+
+
+
+
+
